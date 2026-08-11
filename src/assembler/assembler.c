61f8c8af8c0c7441951a1e59f2105d552193cc71
lkotlus <logan.kotlus@gmail.com>
2026-08-11 01:50:38 -0400

    Switch to going by line

--- src/assembler/assembler.c
+++ src/assembler/assembler.c
@@ -4,7 +4,43 @@
 #include "constants.h"
 #include "line.h"
 
-char get_token(FILE* file, char* token) {
+// Gets the next line that contains actual tokens.
+// Only stores from the first token to the 
+// end of the line or the start of a comment.
+int get_line(FILE* file, char* line) {
+    int ch;
+    int i = 0;
+
+    while (((ch = toupper(fgetc(file))) != EOF) && (isspace((unsigned char)ch) || ch == ';')) {
+        if (ch == ';') {
+            while (((ch = toupper(fgetc(file))) != EOF) && ch != '\n');
+        }
+    }
+
+    if (ch == EOF) {
+        return 0;
+    }
+
+    while (ch != ';' && ch != '\n') {
+        if (ch == EOF) {
+            return 0;
+        }
+        if (i < STRLEN - 1) {
+            line[i++] = (unsigned char)ch;
+        }
+        ch = toupper(fgetc(file));
+    }
+
+    if (ch == ';') {
+        while (((ch = toupper(fgetc(file))) != EOF) && ch != '\n');
+    }
+
+    line[i] = '\0';
+
+    return 1;
+}
+
+int get_token(FILE* file, char* token) {
     int ch;
     int i = 0;
 
@@ -25,16 +61,16 @@ char get_token(FILE* file, char* token) {
     return 1;
 }
 
-char get_line(FILE* file);
-
 void loop(const char* fpath) {
     FILE* file = fopen(fpath, "r");
 
-    char token[STRLEN];
-    while (get_token(file, token)) {
-        Operand* operand = (Operand*)malloc(sizeof(Operand));
-        OperandType parse_success = parse_operand(token, operand);
-        printf("%d, %d, %s\n", parse_success, operand->type, token);
+    //char token[STRLEN];
+    //while (get_token(file, token)) {
+    //    Operand* operand = (Operand*)malloc(sizeof(Operand));
+    //    OperandType parse_success = parse_operand(token, operand);
+    char line[STRLEN];
+    while (get_line(file, line)) {
+        printf("%s\n", line);
     }
 
     fclose(file);
