8ec774e34d565f9982d3f2c2f1f242900627cc4f
lkotlus <logan.kotlus@gmail.com>
2026-08-14 22:05:28 -0400

    We shall soon assemble.

--- src/assembler/token.h
+++ src/assembler/token.h
@@ -27,6 +27,7 @@
 #define OPT_IMM_OFFSET                                                       \
   (OPT_IMM5 | OPT_OFFSET6 | OPT_TRAPVECT8 | OPT_PCOFFSET9 | OPT_PCOFFSET11 | \
    OPT_IMM16)
+#define OPT_REG (OPT_REG1 | OPT_REG2 | OPT_REG3)
 
 typedef struct LabelMap LabelMap;
 struct LabelMap {
@@ -51,14 +52,16 @@ LabelResult parse_label(const char *token, LabelList *labell);
 // Operands
 typedef enum {
   OPT_LAB = 1 << 0,
-  OPT_REG = 1 << 1,
-  OPT_IMM5 = 1 << 2,
-  OPT_PCOFFSET9 = 1 << 3,
-  OPT_PCOFFSET11 = 1 << 4,
-  OPT_OFFSET6 = 1 << 5,
-  OPT_TRAPVECT8 = 1 << 6,
-  OPT_STRINGZ = 1 << 7,
-  OPT_IMM16 = 1 << 8,
+  OPT_REG1 = 1 << 1,
+  OPT_REG2 = 1 << 2,
+  OPT_REG3 = 1 << 3,
+  OPT_IMM5 = 1 << 4,
+  OPT_PCOFFSET9 = 1 << 5,
+  OPT_PCOFFSET11 = 1 << 6,
+  OPT_OFFSET6 = 1 << 7,
+  OPT_TRAPVECT8 = 1 << 8,
+  OPT_STRINGZ = 1 << 9,
+  OPT_IMM16 = 1 << 10,
   OPERAND_INVALID = 0
 } OperandType;
 
@@ -123,41 +126,42 @@ typedef struct {
 } OperationSpec;
 
 typedef struct {
+  uint16_t machine_code;
   const char *token;
   OperationCode opcode;
   OperationSpec opspec;
 } OperationCodeMap;
 
 static const OperationCodeMap opcode_map[] = {
-    {"ADD", ADD, {3, {OPT_REG, OPT_REG, OPT_REG | OPT_IMM5}}},
-    {"AND", AND, {3, {OPT_REG, OPT_REG, OPT_REG | OPT_IMM5}}},
-    {"NOT", NOT, {2, {OPT_REG, OPT_REG, 0}}},
-    {"LD", LD, {2, {OPT_REG, OPT_LAB | OPT_PCOFFSET9, 0}}},
-    {"LDI", LDI, {2, {OPT_REG, OPT_LAB | OPT_PCOFFSET9, 0}}},
-    {"LDR", LDR, {3, {OPT_REG, OPT_REG, OPT_OFFSET6}}},
-    {"LEA", LEA, {2, {OPT_REG, OPT_LAB | OPT_PCOFFSET9, 0}}},
-    {"ST", ST, {2, {OPT_REG, OPT_LAB | OPT_PCOFFSET9, 0}}},
-    {"STR", STR, {3, {OPT_REG, OPT_REG, OPT_OFFSET6}}},
-    {"STI", STI, {2, {OPT_REG, OPT_LAB | OPT_PCOFFSET9, 0}}},
-    {"BR", BR, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
-    {"BRN", BRN, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
-    {"BRZ", BRZ, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
-    {"BRP", BRP, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
-    {"BRNZ", BRNZ, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
-    {"BRZP", BRZP, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
-    {"BRNP", BRNP, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
-    {"JSR", JSR, {1, {OPT_LAB | OPT_PCOFFSET11, 0, 0}}},
-    {"JSRR", JSRR, {1, {OPT_REG, 0, 0}}},
-    {"JMP", JMP, {1, {OPT_REG, 0, 0}}},
-    {"RTI", RTI, {0, {0, 0, 0}}},
-    {"TRAP", TRAP, {1, {OPT_TRAPVECT8, 0, 0}}},
-    {"GETC", GETC, {0, {0, 0, 0}}},
-    {"OUT", OUT, {0, {0, 0, 0}}},
-    {"PUTS", PUTS, {0, {0, 0, 0}}},
-    {"IN", IN, {0, {0, 0, 0}}},
-    {"PUTSP", PUTSP, {0, {0, 0, 0}}},
-    {"HALT", HALT, {0, {0, 0, 0}}},
-    {"", OPCODE_INVALID, {0, {0, 0, 0}}}};
+    {0x1000, "ADD", ADD, {3, {OPT_REG1, OPT_REG2, OPT_REG3 | OPT_IMM5}}},
+    {0x5000, "AND", AND, {3, {OPT_REG1, OPT_REG2, OPT_REG3 | OPT_IMM5}}},
+    {0x903F, "NOT", NOT, {2, {OPT_REG1, OPT_REG2, 0}}},
+    {0x2000, "LD", LD, {2, {OPT_REG1, OPT_LAB | OPT_PCOFFSET9, 0}}},
+    {0xA000, "LDI", LDI, {2, {OPT_REG1, OPT_LAB | OPT_PCOFFSET9, 0}}},
+    {0x6000, "LDR", LDR, {3, {OPT_REG1, OPT_REG2, OPT_OFFSET6}}},
+    {0xE000, "LEA", LEA, {2, {OPT_REG1, OPT_LAB | OPT_PCOFFSET9, 0}}},
+    {0x3000, "ST", ST, {2, {OPT_REG1, OPT_LAB | OPT_PCOFFSET9, 0}}},
+    {0x7000, "STR", STR, {3, {OPT_REG1, OPT_REG2, OPT_OFFSET6}}},
+    {0xB000, "STI", STI, {2, {OPT_REG1, OPT_LAB | OPT_PCOFFSET9, 0}}},
+    {0x0E00, "BR", BR, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
+    {0x0800, "BRN", BRN, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
+    {0x0400, "BRZ", BRZ, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
+    {0x0200, "BRP", BRP, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
+    {0x0C00, "BRNZ", BRNZ, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
+    {0x0600, "BRZP", BRZP, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
+    {0x0A00, "BRNP", BRNP, {1, {OPT_LAB | OPT_PCOFFSET9, 0, 0}}},
+    {0x4800, "JSR", JSR, {1, {OPT_LAB | OPT_PCOFFSET11, 0, 0}}},
+    {0x4000, "JSRR", JSRR, {1, {OPT_REG2, 0, 0}}},
+    {0xC000, "JMP", JMP, {1, {OPT_REG2, 0, 0}}},
+    {0x8000, "RTI", RTI, {0, {0, 0, 0}}},
+    {0xF000, "TRAP", TRAP, {1, {OPT_TRAPVECT8, 0, 0}}},
+    {0xF020, "GETC", GETC, {0, {0, 0, 0}}},
+    {0xF021, "OUT", OUT, {0, {0, 0, 0}}},
+    {0xF022, "PUTS", PUTS, {0, {0, 0, 0}}},
+    {0xF023, "IN", IN, {0, {0, 0, 0}}},
+    {0xF024, "PUTSP", PUTSP, {0, {0, 0, 0}}},
+    {0xF025, "HALT", HALT, {0, {0, 0, 0}}},
+    {0xFFFF, "", OPCODE_INVALID, {0, {0, 0, 0}}}};
 
 OperationCodeMap *parse_opcode(const char *token);
 
